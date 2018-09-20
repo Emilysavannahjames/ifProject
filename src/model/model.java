784c3c922461013d3784fd52c953e0ce8b4d8da8
Emilysavannahjames <43076722+Emilysavannahjames@users.noreply.github.com>
2018-09-20 12:29:51 -0600

    added voids to set

--- src/model/model.java
+++ src/model/model.java
@@ -8,11 +8,20 @@ public class model
 	private int timeHours;
 	private double distance;
 	//-----------------------
-	
+	/**
+	 * Default constructor for a run
+	 * Initializes all variables to valid but "bad" values
+	 * used for later customization
+	 */
 	public model()
 	{
 		//constructor default
 	}
+	/**
+	 * Initializes a name 
+	 * @param name or the run
+	 */
+	
 	public model(String name)
 	{
 		this.name = name;
@@ -40,6 +49,22 @@ public class model
 	public double getDistance()
 	{
 		return distance;
+	}
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+	public void setTimeMinutes(int timeMinutes)
+	{
+		this.timeMinutes = timeMinutes;
+	}
+	public void setTimeHours(int timeHours)
+	{
+		this.timeHours = timeHours;
+	}
+	public void setHasDistance(boolean hasDistance)
+	{
+		this.hasDistance = hasDistance;
 	}
 	
 }
