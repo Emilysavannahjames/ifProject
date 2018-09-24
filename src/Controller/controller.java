e633178b017edb9a7eaa1db8bea7f4a95ef664e3
Emilysavannahjames <43076722+Emilysavannahjames@users.noreply.github.com>
2018-09-24 12:38:00 -0600

    javadoc

--- src/Controller/controller.java
+++ src/Controller/controller.java
@@ -1,6 +1,31 @@
 package Controller;
 
+import model.model; //import section
+import javax.swing.JOptionPane;
+
 public class controller
 {
-
+	/**
+	 * Builds the instance of the controller
+	 */
+	private model myCrossCountry;
+	private model userCrossCountry;
+	//private Scanner inputScanner;
+	
+	public controller()
+	{
+		myCrossCountry = new model("5k", 12, 1, 1.0);
+		userCrossCountry = new model();
+		//inputScanner = new Scanner(System.in);
+	}
+	/**
+	 * This is where the program starts calling methods to run the program
+	 */
+	public void start()
+	{
+		JOptionPane.showMessageDialog(null,  myCrossCountry);
+		System.out.println(myCrossCountry);
+		
+		JOptionPane.showInputDialog(null, )
+	}
 }
